bd578078b678e80a98a93189535792c73fa07460
mfelipearaujo <manoelfelipearaujo@icloud.com>
2025-02-08 11:26:53 -0300

    Completed Single Product Page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,151 @@
+import React from "react"
+import styled from "styled-components"
+import Announcement from "../components/Announcement"
+import Navbar from "../components/Navbar"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
+import { Add, Remove } from "@mui/icons-material"
+
+const Container = styled.div``
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+`
+
+const ImgContainer = styled.div`
+    flex: 1;
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: cover;
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+`
+
+const Title = styled.h1`
+    font-weight: 300;
+`
+
+const Description = styled.p`
+    margin: 10px 0px;
+`
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`
+
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 10px 0px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`
+
+const FilterSize = styled.select`
+    margin-left: 5px;
+    padding: 5px;
+`
+
+const FilterSizeOption = styled.option``
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border: 1px solid #000000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`
+
+const Button = styled.button`
+    padding: 10px 15px;
+    border: 1px solid #000000;
+    background-color: #ffffff;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover {
+        background-color: #c6e7ff;
+    }
+`
+
+const Product = () => {
+    return (
+        <Container>
+            <Announcement />
+            <Navbar />
+            <Wrapper>
+                <ImgContainer>
+                    <Image src='images/joker_tshirt_boy_front.jpg' />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>Joker T-shirt</Title>
+                    <Description>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nunc ac ipsum mattis, dignissim nisl sit amet, consequat
+                        sapien. Proin vehicula quam vitae ultricies pretium.
+                        Morbi non pharetra risus, ac pulvinar quam. Vivamus
+                        gravida congue tellus, ac blandit libero porta eget.
+                        Nunc porttitor diam eget velit venenatis vulputate.
+                    </Description>
+                    <Price>$ 25.00</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Size</FilterTitle>
+                            <FilterSize>
+                                <FilterSizeOption>XS</FilterSizeOption>
+                                <FilterSizeOption>S</FilterSizeOption>
+                                <FilterSizeOption>M</FilterSizeOption>
+                                <FilterSizeOption>L</FilterSizeOption>
+                                <FilterSizeOption>XL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove />
+                            <Amount>1</Amount>
+                            <Add />
+                        </AmountContainer>
+                        <Button>Add to cart</Button>
+                    </AddContainer>
+                </InfoContainer>
+            </Wrapper>
+            <Newsletter />
+            <Footer />
+        </Container>
+    )
+}
+
+export default Product
